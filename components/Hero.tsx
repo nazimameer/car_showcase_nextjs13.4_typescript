70be8213c988e8038740647b88ba82bb8c8199d9
NAZIM AMEER <nazimameerpp@gmail.com>
2023-07-22 19:37:12 +0530

    footer finished

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,11 +27,10 @@ const Hero = () => {
       <div className='hero__image-container'>
         <div className='hero__image'>
           <Image src={`/hero.png`} alt='hero' fill className='object-contain'/>
-
-          <div className='hero__image-overlay'>
-
-          </div>
         </div>
+
+          <div className='hero__image-overlay' />
+
       </div>
     </div>
   )
